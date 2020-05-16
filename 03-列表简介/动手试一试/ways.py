a3c881d347c8d4982e90b671ef1c3a8d0cae9599
xuelang201201 <xuelang201201@gmail.com>
2020-05-17 05:09:11 +0800

    3-3 自己的列表

--- 03-列表简介/动手试一试/ways.py
+++ 03-列表简介/动手试一试/ways.py
@@ -2,6 +2,6 @@
 自己的列表: 想想你喜欢的通勤方式，如骑摩托车或开汽车，并创建一个包含多种通勤方式的列表。
 根据该列表打印一系列有关这些通勤方式的宣言，如 “I would like to own a Honda motorcycle”。
 """
-ways = ['walk', 'bicycle', 'motorcycle', 'car', 'bus', 'subway' ]
+ways = ['walk', 'bicycle', 'motorcycle', 'car', 'bus', 'subway']
 for way in ways:
     print("I would like to go to work by %s." % way)
