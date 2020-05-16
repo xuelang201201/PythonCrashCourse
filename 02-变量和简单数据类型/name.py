6030ccda5b9106da1cde41c0dc9cde0859d28f73
xuelang201201 <xuelang201201@gmail.com>
2020-05-17 03:07:04 +0800

    字符串操作

--- 02-变量和简单数据类型/name.py
+++ 02-变量和简单数据类型/name.py
@@ -14,5 +14,6 @@ full_name = first_name + " " + last_name
 print(full_name)
 print("Hello, %s!" % full_name.title())
 
+# message = "Hello, %s!" % full_name.title()
 message = "Hello, " + full_name.title() + "!"
 print(message)
