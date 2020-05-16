f0c93ad1b33a59d5bed2518fb4ef40e3dc8f4368
xuelang201201 <xuelang201201@gmail.com>
2020-05-17 07:33:11 +0800

    创建数字列表

--- 04-操作列表/squares.py
+++ 04-操作列表/squares.py
@@ -1,9 +1,14 @@
-"""使用 range()创建数字列表"""
+"""创建数字列表"""
 
+"""使用 range()创建数字列表"""
 squares = []
 for value in range(1, 11):
     # square = value ** 2
     # squares.append(square)
     squares.append(value ** 2)
 
+print(squares)
+
+"""列表解析"""
+squares = [value ** 2 for value in range(1, 11)]
 print(squares)
