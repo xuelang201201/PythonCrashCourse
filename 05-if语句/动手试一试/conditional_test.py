c28d4aeac8c73bcfe6eaa278a0e8690cdea63cf5
xuelang201201 <xuelang201201@gmail.com>
2020-05-17 09:46:03 +0800

    5-1 条件测试

---
+++ 05-if语句/动手试一试/conditional_test.py
@@ -0,0 +1,63 @@
+"""
+条件测试：编写一系列条件测试；将每个测试以及你对其结果的预测和实际结果都打印出来。你编写的代码应类似于下面这样：
+————————————————————————————————————————————————————————————————————————————————————————————————————
+    car = 'subaru'
+    print("Is car == 'subaru'? I predict True.")
+    print(car == 'subaru')
+
+    print("\nIs car == 'audi'? I predict False.")
+    print(car == 'audi')
+————————————————————————————————————————————————————————————————————————————————————————————————————
+1.详细研究实际结果，直到你明白了它为何为 True 或 False。
+2.创建至少 10 个测试，且其中结果分别为 True 和 False 的测试都至少有 5 个。
+"""
+
+# from random import randint
+#
+# number = randint(1, 10)
+#
+# while True:
+#     guess = int(input("Guess a number: "))
+#     if number < guess:
+#         print("Too large.")
+#     elif number > guess:
+#         print("Too small.")
+#     else:
+#         print("Correct!")
+#         break
+
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+number = 10
+print("\nIs number == 10? I predict True.")
+print(number == 10)
+
+print("\nIs number == 5? I predict False.")
+print(number == 5)
+
+color = 'blue'
+print("\nIs color == 'blue'? I predict True.")
+print(color == 'blue')
+
+print("\nIs color == 'red'? I predict False.")
+print(color == 'red')
+
+animal = 'dog'
+print("\nIs animal == 'dog'? I predict True.")
+print(animal == 'dog')
+
+print("\nIs animal == 'cat'? I predict False.")
+print(animal == 'cat')
+
+language = 'python'
+print("\nIs language == 'python'? I predict True.")
+print(language == 'python')
+
+print("\nIs language == 'go'? I predict False.")
+print(language == 'go')
