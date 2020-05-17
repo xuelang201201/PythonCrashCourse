a39a380812b281a4f535be603d96a3ebd53ebbea
xuelang201201 <xuelang201201@gmail.com>
2020-05-17 08:38:40 +0800

    4-1 比萨 4-11 你的比萨和我的比萨

--- 04-操作列表/动手试一试/pizzas.py
+++ 04-操作列表/动手试一试/pizzas.py
@@ -10,3 +10,24 @@ for pizza in pizzas:
     print("I like %s pizza." % pizza)
 
 print("I really love pizza!")
+
+"""
+你的比萨和我的比萨：创建上面比萨列表的副本，并将其存储到变量 friend_pizzas 中，再完成如下任务。
+1.在原来的比萨列表中添加一种比萨。
+2.在列表 friend_pizzas 中添加另一种比萨。
+3.核实你有两个不同的列表。为此，打印消息 “My favorite pizzas are:”，再使用一个 for 循环来打印第
+一个列表；打印消息 “My friend's favorite pizzas are:”，再使用一个 for 循环来打印第二个列表。核
+实新增的比萨被添加到了正确的列表中。
+"""
+friend_pizzas = pizzas[:]
+
+pizzas.append('hawaii')
+friend_pizzas.append('seafood')
+
+print("My favorite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+print("My friend's favorite pizzas are:")
+for friend_pizza in friend_pizzas:
+    print(friend_pizza)
