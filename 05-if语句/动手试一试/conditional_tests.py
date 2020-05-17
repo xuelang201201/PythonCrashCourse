0f1221e872f790a132ff9aedf088dd6c3df73742
xuelang201201 <xuelang201201@gmail.com>
2020-05-17 10:37:39 +0800

    5-2 更多的条件测试

--- 05-if语句/动手试一试/conditional_tests.py
+++ 05-if语句/动手试一试/conditional_tests.py
@@ -8,4 +8,46 @@
 5.测试特定的值是否包含在列表中。
 6.测试特定的值是否未包含在列表中。
 """
+language = 'python'
+print("Is language == 'python'? I predict True.")
+print(language == 'python')
+print("\nIs language == 'go'? I predict False.")
+print(language == 'go')
 
+name = 'Sarah'
+print("\nIs name == 'sarah'? I predict True.")
+print(name.lower() == 'sarah')
+print("\nIs name == 'Sarah'? I predict False.")
+print(name == 'sarah')
+
+age = 23
+print("\nIs age == 23? I predict True.")
+print(age == 23)
+print("\nIs age != 23? I predict False.")
+print(age != 23)
+
+print("\nIs age > 22? I predict True.")
+print(age > 22)
+print("\nIs age < 22? I predict False.")
+print(age < 22)
+
+print("\nIs age >= 23? I predict True.")
+print(age >= 23)
+print("\nIs age <= 22? I predict False.")
+print(age <= 22)
+
+print("\nIs language = 'python' or age = 24? I predict True.")
+print(language == 'python' or age == 24)
+print("\nIs language = 'python' and age = 24? I predict False.")
+print(language == 'python' and age == 24)
+
+colors = ['red', 'blue', 'white', 'black', 'yellow']
+print("\nIs 'red' in colors? I predict True.")
+print('red' in colors)
+print("\nIs 'green' in colors? I predict False.")
+print('green' in colors)
+
+print("\nIs 'purple' not in colors? I predict True.")
+print('purple' not in colors)
+print("\nIs 'black' not in colors? I predict False.")
+print('black' not in colors)
