71761ff5f71f4c1a969113d63100bddd14370d9d
xuelang201201 <xuelang201201@gmail.com>
2020-05-23 07:21:19 +0800

    遍历所有的键-值对

--- 06-字典/favorite_languages.py
+++ 06-字典/favorite_languages.py
@@ -8,4 +8,3 @@ favorite_languages = {
 # print("Sarah's favorite language is %s." % favorite_languages['sarah'].title())
 for name, language in favorite_languages.items():
     print(name.title() + "'s favorite language is " + language.title() + ".")
-
