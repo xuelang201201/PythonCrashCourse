1573717f2a703c1bbffa47aca54018e671526d1e
xuelang201201 <xuelang201201@gmail.com>
2020-05-24 05:08:08 +0800

    函数 input() 的工作原理

---
+++ 07-用户输入和while循环/greeter.py
@@ -0,0 +1,7 @@
+# 编写清晰的程序
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+# name = input("Please enter your name: ")
+name = input(prompt)
+print("Hello, " + name + "!")
