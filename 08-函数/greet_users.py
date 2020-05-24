70e1b3ac3541a2a3c62df5391a62e0a8b9c62ca5
xuelang201201 <xuelang201201@gmail.com>
2020-05-24 11:09:04 +0800

    传递参数

---
+++ 08-函数/greet_users.py
@@ -0,0 +1,10 @@
+# 传递参数
+def greet_users(names):
+    """向列表中的每位用户都发出简单的问候"""
+    for name in names:
+        msg = "Hello, %s!" % name.title()
+        print(msg)
+
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
