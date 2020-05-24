259a308e410f1afa98b107f00b39bbcc5b8b61f3
xuelang201201 <xuelang201201@gmail.com>
2020-05-24 09:46:38 +0800

    8-1 消息

---
+++ 08-函数/动手试一试/message.py
@@ -0,0 +1,12 @@
+"""
+消息：编写一个名为 display_message()的函数，它打印一个句子，指出你在本章学的是什么。调用这个函数，确认显示的消息正确无误。
+"""
+
+
+def display_message():
+    msg = "I'm learning to store code in functions."
+    print(msg)
+
+
+if __name__ == '__main__':
+    display_message()
