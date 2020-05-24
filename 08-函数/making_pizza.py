47efbbd0cdfb501dc8a29c2db61a91d25efcbd1b
xuelang201201 <xuelang201201@gmail.com>
2020-05-24 12:24:05 +0800

    将函数存储在模块中

--- 08-函数/making_pizza.py
+++ 08-函数/making_pizza.py
@@ -1,4 +1,21 @@
-import pizza2
+import pizza2  # 导入整个模块
+import pizza2 as p  # 使用 as 给模块指定别名
+
+from pizza2 import make_pizza  # 导入特定的函数
+from pizza2 import make_pizza as mp  # 使用 as 给函数指定别名
+from pizza2 import *  # 导入模块中的所有函数
 
 pizza2.make_pizza(16, 'pepperoni')
 pizza2.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
