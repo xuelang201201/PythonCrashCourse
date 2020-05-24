6d7db9bf2b8b39e905fcf9be0204f37cac867a89
xuelang201201 <xuelang201201@gmail.com>
2020-05-24 09:51:12 +0800

    8-2 喜欢的图书

---
+++ 08-函数/动手试一试/favorite_book.py
@@ -0,0 +1,12 @@
+"""
+喜欢的图书：编写一个名为 favorite_book()的函数，其中包含一个名为 title 的形参。这个函数打印一条消息，
+如 One of my favorite books is Alice in Wonderland。调用这个函数，并将一本图书的名称作为实参传递给它。
+"""
+
+
+def favorite_book(title):
+    msg = "One of my favorite books is %s." % title
+    print(msg)
+
+
+favorite_book('Alice in Wonderland')
