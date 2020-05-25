2ba7521ce3f7366c88aabac552ba6602e47b1f3e
xuelang201201 <xuelang201201@gmail.com>
2020-05-25 12:42:44 +0800

    Python标准库

---
+++ 09-类/favorite_languages.py
@@ -0,0 +1,12 @@
+"""Python标准库"""
+from collections import OrderedDict
+
+favorite_languages = OrderedDict()
+
+favorite_languages['jen'] = 'python'
+favorite_languages['sarah'] = 'c'
+favorite_languages['edward'] = 'ruby'
+favorite_languages['phil'] = 'python'
+
+for name, language in favorite_languages.items():
+    print("%s's favorite language is %s." % (name.title(), language.title()))
