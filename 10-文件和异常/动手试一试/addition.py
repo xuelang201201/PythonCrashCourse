31c0f3468da2612fb6d51d18ff33d5f6ab965516
xuelang201201 <xuelang201201@gmail.com>
2020-05-26 10:28:18 +0800

    10-6 加法运算

--- 10-文件和异常/动手试一试/addition.py
+++ 10-文件和异常/动手试一试/addition.py
@@ -3,3 +3,16 @@
 将引发 ValueError 异常。编写一个程序，提示用户输入两个数字，再将它们相加并打印结果。在用户输入的任何一个值不是数字时都捕获
 ValueError 异常，并打印一条友好的错误消息。对你编写的程序进行测试：先输入两个数字，再输入一些文本而不是数字。
 """
+try:
+    x = input("Give me a number: ")
+    x = int(x)
+
+    y = input("Give me another number: ")
+    y = int(y)
+
+except ValueError:
+    print("Sorry, I really needed a number.")
+
+else:
+    s = x + y
+    print("The sum of %d and %d is %d." % (x, y, s))
