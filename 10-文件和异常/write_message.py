4adc53c47e06945b7311d56e54df8740c8e7fed9
xuelang201201 <xuelang201201@gmail.com>
2020-05-26 09:44:59 +0800

    写入文件

--- 10-文件和异常/write_message.py
+++ 10-文件和异常/write_message.py
@@ -8,4 +8,3 @@ with open(filename, 'w') as file_object:  # 写入空文件和写入多行
 with open(filename, 'a') as file_object:  # 附加到文件
     file_object.write("I also love finding meaning in large datasets.\n")
     file_object.write("I love creating apps that can run in a browser.\n")
-
