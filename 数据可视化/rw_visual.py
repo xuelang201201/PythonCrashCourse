1a8c03fb836414dc66492155639e6178937f9293
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 01:39:14 +0800

    增加点数

--- 数据可视化/rw_visual.py
+++ 数据可视化/rw_visual.py
@@ -9,12 +9,12 @@ from random_walk import RandomWalk
 # 只要程序处于活动状态，就不断模拟随机漫步
 while True:
     # 创建一个RandomWalk实例，并将其包含的点都绘制出来
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
     # 给点着色
-    point_number = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_number, cmap=plt.cm.Blues, s=15)
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, s=1)
 
     # 突出起点和终点
     plt.scatter(0, 0, c='green', s=100)
