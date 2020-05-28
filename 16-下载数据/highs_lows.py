33267562a7d86e2dbc177f9ea15c530e51e9253c
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 04:26:35 +0800

    提取并读取数据

--- 16-下载数据/highs_lows.py
+++ 16-下载数据/highs_lows.py
@@ -10,3 +10,11 @@ with open(filename) as f:
     # 打印文件头及其位置
     for index, column_header in enumerate(header_row):
         print(index, column_header)
+
+    # 从文件中获取最高气温
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+    print(highs)
