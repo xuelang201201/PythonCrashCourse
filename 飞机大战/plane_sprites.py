8b603daccb4640987946e563ef6ff0eba566a54e
xuelang201201 <xuelang201201@gmail.com>
2020-05-28 21:46:09 +0800

    定义子弹类

--- 飞机大战/plane_sprites.py
+++ 飞机大战/plane_sprites.py
@@ -108,3 +108,24 @@ class Hero(GameSprite):
 
     def fire(self):
         print("发射子弹...")
+
+
+class Bullet(GameSprite):
+    """子弹精灵"""
+
+    def __init__(self):
+
+        # 调用父类方法，设置子弹图片，设置初始速度
+        super(Bullet, self).__init__("./images./bullet1.png", -2)
+
+    def update(self):
+
+        # 调用父类方法，让子弹沿垂直方向飞行
+        super(Bullet, self).update()
+
+        # 判断子弹是否飞出屏幕
+        if self.rect.bottom < 0:
+            self.kill()
+
+    def __del__(self):
+        print("子弹被销毁...")
