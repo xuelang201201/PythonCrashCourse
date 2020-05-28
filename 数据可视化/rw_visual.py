6e3de079a1998050074ae84ce235b0249d4861b8
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 01:37:28 +0800

    隐藏坐标轴

--- 数据可视化/rw_visual.py
+++ 数据可视化/rw_visual.py
@@ -20,6 +20,10 @@ while True:
     plt.scatter(0, 0, c='green', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', s=100)
 
+    # 隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     # 模拟多次随机漫步
