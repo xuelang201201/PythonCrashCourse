05104d8c2759acaeedaf0bae45c158c257d8f7ed
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 04:55:12 +0800

    再绘制一个数据系列

--- 16-下载数据/highs_lows.py
+++ 16-下载数据/highs_lows.py
@@ -3,8 +3,9 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
+# 从文件中获取日期、最高气温和最低气温
+filename = 'sitka_weather_2014.csv'
 # filename = 'sitka_weather_07-2014.csv'
-filename = 'venv/sitka_weather_2014.csv'
 with open(filename) as f:
     # 分析CSV文件头
     reader = csv.reader(f)
@@ -17,12 +18,17 @@ with open(filename) as f:
 
     # 从文件中获取日期和最高气温
     # highs = []
-    dates, highs = [], []
+    # dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[0], "%Y-%m-%d")
         dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
+
+        low = int(row[3])
+        lows.append(low)
 
     # print(highs)
 
@@ -30,10 +36,12 @@ with open(filename) as f:
     fig = plt.figure(dpi=128, figsize=(10, 6))
     # plt.plot(highs, c='red')
     plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue')
 
     # 设置图形的格式
     # plt.title("Daily high temperatures, July 2014", fontsize=24)
-    plt.title("Daily high temperatures - 2014", fontsize=24)
+    # plt.title("Daily high temperatures - 2014", fontsize=24)
+    plt.title("Daily high and low temperatures - 2014", fontsize=24)
     plt.xlabel('', fontsize=16)
     fig.autofmt_xdate()  # 绘制斜的日期标签
     plt.ylabel("Temperature (F)", fontsize=16)
