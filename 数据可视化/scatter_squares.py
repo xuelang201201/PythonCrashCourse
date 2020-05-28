33c004a1d3724d62a7b8421dde7e2319128b1226
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 00:41:28 +0800

    自动计算数据

--- 数据可视化/scatter_squares.py
+++ 数据可视化/scatter_squares.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+# x_values = [1, 2, 3, 4, 5]
+x_values = list(range(1, 1001))
+# y_values = [1, 4, 9, 16, 25]
+y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=100)
+# plt.scatter(x_values, y_values, s=100)
+plt.scatter(x_values, y_values, s=40)
 
 # 设置图表标题并给坐标轴加上标签
 plt.title("Square Numbers", fontsize=24)
@@ -12,5 +15,8 @@ plt.ylabel("Square of Value", fontsize=14)
 
 # 设置刻度标记的大小
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+# 设置每个坐标轴的取值范围
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
