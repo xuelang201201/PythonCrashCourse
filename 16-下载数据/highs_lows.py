e9f0ba3ccd1b76767666e40542a113fcb75d8023
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 05:03:37 +0800

    错误检查

--- 16-下载数据/highs_lows.py
+++ 16-下载数据/highs_lows.py
@@ -22,14 +22,16 @@ with open(filename) as f:
     # dates, highs = [], []
     dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-
-        high = int(row[1])
-        highs.append(high)
-
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
     # print(highs)
 
@@ -43,7 +45,9 @@ with open(filename) as f:
     # 设置图形的格式
     # plt.title("Daily high temperatures, July 2014", fontsize=24)
     # plt.title("Daily high temperatures - 2014", fontsize=24)
-    plt.title("Daily high and low temperatures - 2014", fontsize=24)
+    # plt.title("Daily high and low temperatures - 2014", fontsize=24)
+    title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+    plt.title(title, fontsize=20)
     plt.xlabel('', fontsize=16)
     fig.autofmt_xdate()  # 绘制斜的日期标签
     plt.ylabel("Temperature (F)", fontsize=16)
