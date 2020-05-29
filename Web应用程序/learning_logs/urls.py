0eaf596e79f4754eb7e00cc9fdac4b68b5e99994
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 11:48:08 +0800

    显示所有主题的页面

--- Web应用程序/learning_logs/urls.py
+++ Web应用程序/learning_logs/urls.py
@@ -6,5 +6,8 @@ from . import views
 
 urlpatterns = [
     # 主页
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+
+    # 显示所有的主题
+    url(r'^topics/$', views.topics, name='topics'),
 ]
