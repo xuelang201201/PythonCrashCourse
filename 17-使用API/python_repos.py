6fdd2411d12f425e9484dfba8b97fd7afde2f1c6
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 08:44:25 +0800

    根据数据绘图

--- 17-使用API/python_repos.py
+++ 17-使用API/python_repos.py
@@ -20,7 +20,8 @@ repo_dicts = response_dict['items']
 
 # print("\nSelected information about first repository:")
 # print("\nSelected information about each repository:")
-names, stars = [], []
+# names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     # print('Name:', repo_dict['name'])
     # print('Owner:', repo_dict['owner']['login'])
@@ -30,7 +31,14 @@ for repo_dict in repo_dicts:
     # print('Updated:', repo_dict['updated_at'])
     # print('Description:', repo_dict['description'])
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    # stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description'])
+    }
+    plot_dicts.append(plot_dict)
+
 
 # 可视化
 my_style = LS('#333366', base_style=LCS)
@@ -49,5 +57,6 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+# chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
