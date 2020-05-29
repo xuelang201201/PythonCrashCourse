c8047a87a71340c22d67dbb73efa6a87bb539f05
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 08:22:19 +0800

    概述最受欢迎的仓库

--- 17-使用API/python_repos.py
+++ 17-使用API/python_repos.py
@@ -13,14 +13,16 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
-# 研究第一个仓库
-repo_dict = repo_dicts[0]
+# # 研究第一个仓库
+# repo_dict = repo_dicts[0]
 
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+# print("\nSelected information about first repository:")
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
