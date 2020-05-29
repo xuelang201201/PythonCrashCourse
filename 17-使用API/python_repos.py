23f3f13eb9f804e53f224eb1b303a4389e963ae0
xuelang201201 <xuelang201201@gmail.com>
2020-05-29 08:48:55 +0800

    在图表中添加可单击的链接

--- 17-使用API/python_repos.py
+++ 17-使用API/python_repos.py
@@ -35,7 +35,8 @@ for repo_dict in repo_dicts:
 
     plot_dict = {
         'value': repo_dict['stargazers_count'],
-        'label': str(repo_dict['description'])
+        'label': str(repo_dict['description']),
+        'xlink': repo_dict['html_url'],
     }
     plot_dicts.append(plot_dict)
 
