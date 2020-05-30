034d9aff484121bdd60476455251890702d50e09
xuelang201201 <xuelang201201@gmail.com>
2020-05-30 12:21:11 +0800

    保护页面edit_entry

--- Web应用程序/learning_logs/views.py
+++ Web应用程序/learning_logs/views.py
@@ -77,6 +77,8 @@ def edit_entry(request, entry_id):
     """编辑既有条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         # 初次请求，使用当前条目填充表单
