3a77720f22c0def8e2660dc7e7d4141179221ccc
xuelang201201 <xuelang201201@gmail.com>
2020-05-30 11:33:50 +0800

    注册页面的URL模式

--- Web应用程序/users/urls.py
+++ Web应用程序/users/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
     # 注销
     url(r'^logout/$', views.logout_view, name='logout'),
+    # 注册页面
+    url(r'^register/$', views.register, name='register'),
 ]
