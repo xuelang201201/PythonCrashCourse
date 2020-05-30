ac63af3b91f1c5ef17076b944c3c74e55d79d16d
xuelang201201 <xuelang201201@gmail.com>
2020-05-30 10:35:52 +0800

    用于添加新条目的表单

--- Web应用程序/learning_logs/forms.py
+++ Web应用程序/learning_logs/forms.py
@@ -13,7 +13,7 @@ class TopicForm(forms.ModelForm):
 
 class EntryForm(forms.ModelForm):
     class Meta:
-        model = Topic
+        model = Entry
         fields = ['text']
         labels = {'text': ''}
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
