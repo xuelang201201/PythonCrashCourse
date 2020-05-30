fb553c35370b7d5ff15c9e66d0406cf9bfab5f9c
xuelang201201 <xuelang201201@gmail.com>
2020-05-30 08:22:09 +0800

    用于添加新条目的表单

--- Web应用程序/learning_logs/forms.py
+++ Web应用程序/learning_logs/forms.py
@@ -1,7 +1,7 @@
 """用于添加主题的表单"""
 from django import forms
 
-from .models import Topic
+from .models import Topic, Entry
 
 
 class TopicForm(forms.ModelForm):
@@ -9,3 +9,11 @@ class TopicForm(forms.ModelForm):
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
