b412d0eaed1ec48eb3ef364e604d506adaa51529
xuelang201201 <xuelang201201@gmail.com>
2020-05-30 11:30:16 +0800

    视图函数logout_view()

--- Web应用程序/users/views.py
+++ Web应用程序/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
+
+
+def logout_view(request):
+    """注销用户"""
+    logout(request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
