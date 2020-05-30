2c26b80451e32178d5ca63566c9b96a1bfa24907
xuelang201201 <xuelang201201@gmail.com>
2020-05-30 14:12:37 +0800

    为部署到 Heroku 而修改 settings.py

--- Web应用程序/learning_log/settings.py
+++ Web应用程序/learning_log/settings.py
@@ -135,3 +135,24 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
     'include_jquery': True,
 }
+
+# Heroku设置
+cwd = os.getcwd()
+if cwd == '/app' or cwd[:4] == '/tmp':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+
+    # 让request.is_secure()承认X-Forwarded-Proto头
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # 支持所有的主机头（host header）
+    ALLOWED_HOSTS = ['*']
+
+    # 静态资产配置
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
